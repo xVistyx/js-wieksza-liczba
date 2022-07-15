b7d367759e3c99846ed0d72915b7169ddb423d13
xVistyx <wiktor.hc@gmail.com>
2022-07-15 13:09:19 +0200

    js-wieksza-liczba

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let num1 = 2;
+let num2 = 5;
+
+function wieksza_liczba(_par1, _par2) {
+
+    let wieksza = Math.max(_par1, _par2);
+
+    return wieksza;
+
+}
+
+console.log(wieksza_liczba(num1, num2));
+console.log(wieksza_liczba(12, 8));
